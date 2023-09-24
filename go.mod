6332541f1d8f33cab6d484b240db553aeca7d307
liurui <liurui@xiaoduotech.com>
2023-09-24 18:58:21 +0800

    add go mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Cauchy007/learn_module
+
+require github.com/Cauchy007/learn_go v0.0.1
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import cmd "github.com/Cauchy007/learn_go/hello/hello_new"
+
+func main() {
+
+	cmd.HelloNew()
+}
